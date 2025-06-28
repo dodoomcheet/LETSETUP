b7fa954cadedf0242e0c71881fb143fb8ccb0138
dodoomcheet <53454864+dodoomcheet@users.noreply.github.com>
2025-06-28 14:44:17 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,18 @@
-import webbrowser
-chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-urllist = [
-    "https://www.google.com/",
-    "https://www.naver.com/"
-    ]
-for url in urllist:
-    webbrowser.get(chrome_path).open(url)
+options = Options()
+service = Service(r"C:\Users\CKIRUser\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+drivers = [webdriver.Chrome(service=service, options=options),webdriver.Chrome(service=service, options=options),webdriver.Chrome(service=service, options=options)]
+
+urls = ["https://www.youtube.com/watch?v=mIYzp5rcTvU","https://www.github.com"]
+i = 0
+for url in urls : # URL 띄우기
+    i+=1
+    drivers[i].set_window_size(100,100)  # 창 크기 조절
+    drivers[i].set_window_position(0, i * 100)
+    drivers[i].get(url)
+
+i = 0
+input("press any key to end")
